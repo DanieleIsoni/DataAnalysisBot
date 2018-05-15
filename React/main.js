b3e04eb912ba8befc10151198ec3ade4d9d7e87f
DanieleIsoni <daniso1996@gmail.com>
2018-05-15 14:07:35 +0200

    Added menu bar in electron

--- React/main.js
+++ React/main.js
@@ -1,19 +1,24 @@
-const electron = require('electron')
+const electron = require('electron');
+const defaultMenu = require('electron-default-menu');
+const {app, BrowserWindow, Menu, shell} = require('electron');
+const path = require('path');
+const url = require('url');
+  
+function createWindow () {
 
-const {app, BrowserWindow} = require('electron')
-  const path = require('path')
-  const url = require('url')
-  
-  function createWindow () {
+    const menu = defaultMenu(app, shell);
+
+    Menu.setApplicationMenu(Menu.buildFromTemplate(menu));
+
     // Create the browser window.
-    win = new BrowserWindow({width: 800, height: 600})
-  
+    win = new BrowserWindow({width: 800, height: 600});
+
     // e viene caricato il file index.html della nostra app.
     win.loadURL(url.format({
-      pathname: path.join(__dirname, 'index.html'),
-      protocol: 'file:',
-      slashes: true
-    }))
-  }
-  
-  app.on('ready', createWindow)
+        pathname: path.join(__dirname, 'index.html'),
+        protocol: 'file:',
+        slashes: true
+    }));
+}
+
+app.on('ready', createWindow);
