4db5b06f878b8d9a0aaec67abff84795ac5d34b5
simoberny <simone.bernabe@studenti.unitn.it>
2018-05-28 14:11:02 +0200

    Aggiunto basic description tra gli aiuti

--- React/js/Constants/Actions.js
+++ React/js/Constants/Actions.js
@@ -11,9 +11,11 @@ let actions = {
                             <div>I want the <span className="operation">  Operation  </span> of <span className="attribute">  Attribute  </span></div>, 
                             <div> Give me the <span className="operation">  Operation  </span> of <span className="attribute">  Attribute  </span></div>, 
                             <div><span className="operation">  Operation  </span> of  <span className="attribute">  Attribute  </span></div>]},
-        {name: <Translate id="plot">PLotting</Translate>, esempi: [<div>Can you plot the <span className="operation">  Operation  </span> <span className="attribute">  Attribute  </span> by <span className="attribute">  Attribute  </span> </div>]}
+        {name: <Translate id="plot">Plotting</Translate>, esempi: [<div>Can you plot the <span className="operation">  Operation  </span> <span className="attribute">  Attribute  </span> by <span className="attribute">  Attribute  </span> </div>]},
+        {name:<Translate id="basic">Basic Description</Translate>, esempi: [<div>Basic Description</div>] }
     ],
     "after_analisys": [
+        {name:<Translate id="basic">Basic Description</Translate>, esempi: [<div>Basic Description</div>] },
         {name: <Translate id="ana_cont">Analysis</Translate>, esempi: [<div>Now the <span className="operation">  Operation  </span></div>, <div>Of the <span className="attribute">  Attribute  </span></div>]},
         {name: <Translate id="plot">Plotting</Translate>, esempi: [<div>Can you plot the <span className="operation">  Operation  </span> <span className="attribute">  Attribute  </span> by <span className="attribute">  Attribute  </span></div>]}
     ]
