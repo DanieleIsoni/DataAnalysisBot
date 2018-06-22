581f85b27645b4882f78b116d6dbb2f547a9f324
simoberny <simone.bernabe@studenti.unitn.it>
2018-06-22 13:25:47 +0200

    Fix electron

--- React/main.js
+++ React/main.js
@@ -10,7 +10,7 @@ function createWindow () {
     Menu.setApplicationMenu(Menu.buildFromTemplate(menu));
 
     // Create the browser window.
-    win = new BrowserWindow({width: 800, height: 600, icon: __dirname + '/dist/img/logov2.png'});
+    win = new BrowserWindow({width: 800, height: 600, icon: __dirname + '/dist/img/logo.png'});
     win.maximize();
 
     // e viene caricato il file index.html della nostra app.
