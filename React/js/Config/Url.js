cbf8dafe14243718d9df9ac501f34f62173361b7
simoberny <simone.bernabe@studenti.unitn.it>
2018-07-10 12:00:58 +0200

    Url heroku

--- React/js/Config/Url.js
+++ React/js/Config/Url.js
@@ -1,3 +1,3 @@
 import React from "react";
-export const URL_HEROKU = "https://dab.serveo.net";//"https://dab.serveo.net";
+export const URL_HEROKU = "https://data-analysis-bot.herokuapp.com";//"https://dab.serveo.net";
 export const UrlContext = React.createContext(URL_HEROKU);   
